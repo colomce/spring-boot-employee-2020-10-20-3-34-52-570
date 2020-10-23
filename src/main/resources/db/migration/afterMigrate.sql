ae1c4e3da3052cc96ceae56f36a0fa70a13fe48b
Cedric Don Coloma <colomce@oocl.com>
2020-10-23 16:33:24 +0800

    [EmployeeIT][Cedric-Joseph] feature: Added delete statement for company and employee

--- src/main/resources/db/migration/afterMigrate.sql
+++ src/main/resources/db/migration/afterMigrate.sql
@@ -1,3 +1,6 @@
+DELETE FROM EMPLOYEE;
+DELETE FROM COMPANY;
+
 INSERT INTO COMPANY (id, company_name) VALUES (1, 'OOCL');
 
 INSERT INTO EMPLOYEE (id, name, age, gender, salary, company_id) VALUES (1, 'Cedric', 19, 'male', 1000000, 1);
