b14f26c6b5def5d227eaa28b560ebb9c5cbff602
MANALCH <manalch@oocl.com>
2020-10-23 11:56:22 +0800

    [Exception][Cedric-Joseph] feature: implement feature to handle employee not found exception

--- src/main/java/com/thoughtworks/springbootemployee/response/ErrorResponse.java
+++ src/main/java/com/thoughtworks/springbootemployee/response/ErrorResponse.java
@@ -1,11 +1,19 @@
 package com.thoughtworks.springbootemployee.response;
 
 public class ErrorResponse {
+    private String message;
+    private String status;
+
+    public ErrorResponse(String message, String status) {
+        this.message = message;
+        this.status = status;
+    }
+
     public String getMessage() {
-        return null;
+        return message;
     }
 
     public String getStatus() {
-        return null;
+        return status;
     }
 }
