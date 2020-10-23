daff6d8797142e4fbbbd316e4bb459c1911f1015
MANALCH <manalch@oocl.com>
2020-10-23 16:15:59 +0800

    [Flyway][Cedric-Joseph] feature: applied versioned migration scripts and after migrate script

---
+++ src/main/resources/db/migration/V20201023155357__init_db.sql
@@ -0,0 +1,13 @@
+Create table EMPLOYEE (
+    id INT auto_increment,
+    name VARCHAR(255),
+    age INT,
+    gender VARCHAR(255),
+    salary INT,
+    company_id INT
+);
+
+Create table COMPANY (
+    id INT auto_increment,
+    company_name VARCHAR(255)
+);
